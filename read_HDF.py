e52a7917ce820bdc947cf8fe787c89ed90a4a6bc
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-04-19 22:58:14 -0600

    Added comments and sample save

--- read_HDF.py
+++ read_HDF.py
@@ -3,9 +3,14 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    filename = 'L1_files/Q2011079034400.L1A_SCI'
+    # Select HDF5 file to be read in with h5py and save to h5py object
+    filename = '../L1_files/Q2011079034400.L1A_SCI'
     f = h5py.File(filename, 'r')
 
+    # Iterate through groups (heirarchical data structures in our h5py object)
+    #   and append each group to a list.
+    # Save each dataset in group to dataset
+    # Appending each group and dataset to lists just for future reference
     group_key_list = []
     dataset_key_list = []
     for i in xrange(len(f.keys())):
@@ -16,3 +21,7 @@ if __name__ == '__main__':
             print("{0}/{1}".format(group_key, item))
             exec(item + "= np.array(list(f[group_key][item]))")
             dataset_key_list.append(item)
+
+    # Saving 1st antenna beam (4th dim) and H polarization (5th dim) to
+    #   simplify initial test input
+    radiom_lavg_pol0_H = radiom_lavg[:,:,:,0,3]
